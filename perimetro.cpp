15833c582e419a89f886e5de0aa03e6fdf02b6f6
Antonio Presuel <aapresuelrosado@gmail.com>
2022-08-12 01:44:06 -0500

    C++ perimetro - repetitivo

--- perimetro.cpp
+++ perimetro.cpp
@@ -4,21 +4,26 @@ using namespace std;
 int main(){
 	//
 	double ladoA, ladoB, perimetro;
+	int respuesta;
     cout << "Calcular el perimetro de un rectangulo.\n";
-	cout << "Lado A: ";
-	cin >> ladoA;
-	if( ladoA > 0){
-		cout << "Lado B: ";
-		cin >> ladoB;
-		if( ladoB > 0 ){
-			perimetro= 2*ladoA+2*ladoB;
-			cout << "Perimetro= " << perimetro;
+	cout << "Cuantas veces desea repetir el codigo? ";
+	cin >> respuesta;
+	for( int i=0; i<respuesta; i++){
+		cout << "Lado A: ";
+		cin >> ladoA;
+		if( ladoA > 0){
+			cout << "Lado B: ";
+			cin >> ladoB;
+			if( ladoB > 0 ){
+				perimetro= 2*ladoA+2*ladoB;
+				cout << "Perimetro= " << perimetro << "\n\n";
+			}else{
+				cout << "Error en el lado B.\n\n";
+			}
 		}else{
-			cout << "Error en el lado B.\n";
+			cout << "Error en el lado A.\n\n";
 		}
-	}else{
-		cout << "Error en el lado A.\n";
-	}
+	}	
 	//
 	return 0;
 }

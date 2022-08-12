5c9a44bb10e11d836febe8ade6be0c8c869d91e2
Antonio Presuel <aapresuelrosado@gmail.com>
2022-08-12 01:46:46 -0500

    Java perimetro - repetitivo

--- perimetro.java
+++ perimetro.java
@@ -4,21 +4,23 @@ public class perimetro {
 	public static void main (String[] args){
 		Scanner scan= new Scanner(System.in);
 		//
-		System.out.println("Calcular el perimetro de un rectangulo.\n");
-		double ladoA, ladoB, perimetro;
-		System.out.println("Lado A: ");
-		ladoA= scan.nextDouble();
-		if( ladoA > 0 ){
-			System.out.println("Lado B: ");
-			ladoB= scan.nextDouble();
-			if( ladoB > 0 ){
-				perimetro= 2*ladoA+2*ladoB;
-				System.out.println("Perimetro= " + perimetro);
+		for(int i=0; i<3; i++){
+			System.out.println("Calcular el perimetro de un rectangulo.\n");
+			double ladoA, ladoB, perimetro;
+			System.out.println("Lado A: ");
+			ladoA= scan.nextDouble();
+			if( ladoA > 0 ){
+				System.out.println("Lado B: ");
+				ladoB= scan.nextDouble();
+				if( ladoB > 0 ){
+					perimetro= 2*ladoA+2*ladoB;
+					System.out.println("Perimetro= " + perimetro);
+				}else{
+					System.out.println("Error en lado B.\n");
+				}
 			}else{
-				System.out.println("Error en lado B.\n");
+				System.out.println("Error en lado A.\n");
 			}
-		}else{
-			System.out.println("Error en lado A.\n");
 		}
 		//
 	}
